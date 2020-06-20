b6c9cfeb46762f948240643ecadc9f9d255f4f22
Russell Sutter <russell.sutter@gmail.com>
2020-06-20 19:30:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,10 +28,9 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else
-      return false
     end
   end
+  return false
 end
 
 def full?(board)
